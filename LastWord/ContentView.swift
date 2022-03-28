8bfa1ce3e697974aad7ebcbcf39bb587d20454a1
enfrison <98891923+enfrison@users.noreply.github.com>
2022-03-28 11:42:01 -0400

    Created The Heading As Well as the Background rectangle for the Text Options

--- LastWord/ContentView.swift
+++ LastWord/ContentView.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-          
+        ZStack{
+            Color("LW Orange")
+                .edgesIgnoringSafeArea(.all
+                )
+            VStack {
+                Text("LastWord")
+                    .font(Font.custom("Georgia", size: 45))
+                    .italic()
+                    .fontWeight(.regular)
+                    .foregroundColor(Color("LW Cream"))
+                    .multilineTextAlignment(.leading)
+                    .shadow(color: Color("LW Burgundy").opacity(2), radius: 5, x: 5
+                            , y: 10)
+                .padding([.bottom, .trailing], 100.0)
+                Text("Enter Your Options")
+                    .font(Font.custom("Georgia", size: 25))
+                    .foregroundColor(Color("LW Cream"))
+                ZStack{
+                    Rectangle()
+                        .padding(.horizontal)
+                        .foregroundColor(Color("LW CreamOrange"))
+                    
+                }
+            }
+        }
+
     }
 }
 
