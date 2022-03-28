435e950fe5348d1427b3920f14c69d4cef6820fc
enfrison <98891923+enfrison@users.noreply.github.com>
2022-03-28 10:31:07 -0400

    Initial Commit

---
+++ LastWord/LastWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastWordApp.swift
+//  LastWord
+//
+//  Created by Erika Frison on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct LastWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
